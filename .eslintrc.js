153453a00f4148cf2490a2852c8f7aa92d793d5a
Fredy Daniel <fredyd900@gmail.com>
2021-12-10 15:51:35 +0530

    error controll

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,7 @@ module.exports = {
   plugins: ['prettier'],
   rules: {
     'no-console': 'off',
+    'no-param-reassign': ['error', { props: false }],
+    'no-unused-vars': 'off',
   },
 };
--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require('express');
 const dotenv = require('dotenv');
 
 const userRoute = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 dotenv.config({ path: './.env' });
 
 const app = express();
 
 app.use('/api/v1/users', userRoute);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Cant find the ${req.originalUrl} on the server`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
